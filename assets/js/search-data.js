f745b5d897f6b8129af2c80c608a19eb18f3cd36
LeMathE <ibethelo22@gmail.com>
2025-10-04 21:45:56 +0000

    Deploying to gh-pages from @ LeMathE/LeMathE.github.io@4d8d5c42d62ee2415c1bbb481565e9f42ac50347 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -392,16 +392,16 @@ ninja.data = [{
           section: "News",},{id: "news-ready-for-part-iii-mathematics-journey",
           title: 'Ready for Part III Mathematics journey!',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-hubbard-tree-and-entropy",
+          title: 'Hubbard Tree and Entropy',
+          description: "2024 Summer research on Complex Dynamics at Warwick",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/24Warwick/";
+            },},{id: "projects-p-adic-and-newton-39-s-failure",
+          title: 'p-adic and Newton&amp;#39;s failure',
+          description: "2025 Summer research on Arithmetic Dynamics at Cambridge",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
+              window.location.href = "/projects/25Cambridge/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
